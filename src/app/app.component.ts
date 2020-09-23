845028e311da9f41c0d93906f9c5ad1716f1f8a8
Amine KERKOUB <kerk.amine@gmail.com>
2020-09-23 10:03:24 +0200

    tp Collègues #2/structure & composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {collegueMock} from './mock/collegues.mock';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  collegueExemple=collegueMock;
   title = 'collegues-front';
 }
---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,9 @@
+
+import {Collegue} from "../models/Collegue"
+
+export let collegueMock = new Collegue("XXXXXXXXXX","Bob","Eponge","bob.eponge@mer.com",new Date(1990,16,3),'https://statics.lesinrocks.com/content/thumbs/uploads/2013/11/width-1125-height-612-quality-10/bob.jpg.webp');
+
+
+
+
+
