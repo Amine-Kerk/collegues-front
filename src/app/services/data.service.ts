d6cc8033a0b4038a7deb670eca45bd734d535412
Amine KERKOUB <kerk.amine@gmail.com>
2020-09-30 13:27:08 +0200

    tentaive num 5 correction build

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { CreerCollegueForm } from './../creer-collegue/creer-collegue-form';
-import { PhotoDTO } from '../models/PhotoDTO';
+import { PhotoDTO } from './../models/photoDTO';
 
 
 interface CollegueBack {
